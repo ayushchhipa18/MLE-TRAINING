65555e2065b45b3fb7dfb73cdc7c35b66cf8042a
Ayush Chhipa <ayushchhipa18@gmail.com>
2026-01-04 02:17:29 -0800

    correct the path

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -9,8 +9,8 @@ from predict import Predictor
 
 def test_predictor_load():
     p = Predictor(
-        preprocessor_path="/home/ayush/ishu/MLE-TRAINING/preprocessor.pkl",
-        model_path="/home/ayush/ishu/MLE-TRAINING/model.pkl",
+        preprocessor_path="/home/ayush/ishu/MLE-TRAINING/models/preprocessor.pkl",
+        model_path="/home/ayush/ishu/MLE-TRAINING/models/model.pkl",
     )
     assert p is not None
 
@@ -22,8 +22,8 @@ def test_predict_single_row(monkeypatch):
         return dummy_pred
 
     p = Predictor(
-        preprocessor_path="/home/ayush/ishu/MLE-TRAINING/preprocessor.pkl",
-        model_path="/home/ayush/ishu/MLE-TRAINING/model.pkl",
+        preprocessor_path="/home/ayush/ishu/MLE-TRAINING/models/preprocessor.pkl",
+        model_path="/home/ayush/ishu/MLE-TRAINING/models/model.pkl",
     )
 
     monkeypatch.setattr(p.model, "predict", mock_predict)
