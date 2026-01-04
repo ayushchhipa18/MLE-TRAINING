9c529d3e62bc7bc1cd9e994e1e6f82e7439bf2a3
Ayush Chhipa <ayushchhipa18@gmail.com>
2026-01-04 02:11:36 -0800

    fix(test): import patch for mocking model load

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,7 @@
 import json
 import sys, os
+from unittest.mock import patch
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) 
 
 from fastapi.testclient import TestClient
